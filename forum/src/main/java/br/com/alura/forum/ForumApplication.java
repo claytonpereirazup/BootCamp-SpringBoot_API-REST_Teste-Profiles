f24d7ab5ebd32c8b45ff36df2efa62275fbf161b
Clayton Pereira <clayton.pereira@zup.com.br>
2021-06-29 21:58:47 -0300

    Project - HIROKU/DOCKER Enviroment Implemented for Deploy

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,6 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -13,15 +11,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableCaching //Habilita o Springa para utilização de Cache
 @EnableSpringDataWebSupport //Habilita acesso dos parametros de paginacao do Pageable
 @EnableSwagger2 //Documentação automatica
-public class ForumApplication extends SpringBootServletInitializer {
+public class ForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ForumApplication.class);
-	}
-
 }
+
+//public class ForumApplication extends SpringBootServletInitializer {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ForumApplication.class, args);
+//	}
+//	
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(ForumApplication.class);
+//	}
+//
+//}
